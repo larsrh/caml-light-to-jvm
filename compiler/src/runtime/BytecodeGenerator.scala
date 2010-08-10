b7cdad1b85014336275102eec8be67af156a833f
Marek Kubica <marek@xivilization.net>
2010-08-10 10:56:27 +0200

    Write the bytes in, you know, a correct way
    Now we can actually generate bytecode that actually runs and does things.

--- compiler/src/runtime/BytecodeGenerator.scala
+++ compiler/src/runtime/BytecodeGenerator.scala
@@ -1,5 +1,5 @@
 package runtime
-import java.io.{FileWriter, BufferedWriter}
+import java.io.FileOutputStream
 import org.objectweb.asm._
 import org.objectweb.asm.Opcodes._
 import org.objectweb.asm.Type._
@@ -12,25 +12,45 @@ object BytecodeGenerator {
 		}
 	}
 	*/
-	def generateMain(): String = {
-		val `class` = new ClassWriter(0)
-		`class`.visit(V1_5, ACC_PUBLIC + ACC_STATIC, "Foo", null,
-									"java/lang/Object", new Array[String](0))
+	def generateClass(): Array[Byte] = {
+		val `class` = new ClassWriter(ClassWriter.COMPUTE_MAXS +
+																	ClassWriter.COMPUTE_FRAMES)
+		`class`.visit(V1_5, ACC_PUBLIC + ACC_SUPER, "Foo", null,
+									"java/lang/Object", null)
+
+		// does not seem to be neccessary, yet the java compiler generates this
+		/*
+		val init = `class`.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null)
+		init.visitCode()
+		init.visitVarInsn(ALOAD, 0)
+		init.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V")
+		init.visitInsn(RETURN)
+		init.visitMaxs(1, 1)
+		init.visitEnd()
+		*/
+
 		val main = `class`.visitMethod(ACC_PUBLIC + ACC_STATIC, "main",
 																	 "([Ljava/lang/String;)V", null, null)
 		main.visitCode()
-		main.visitMaxs(1, 1)
+		main.visitFieldInsn(GETSTATIC, "java/lang/System", "out",
+												"Ljava/io/PrintStream;")
+		main.visitLdcInsn("Hello World")
+		main.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println",
+												 "(Ljava/lang/String;)V")
+		main.visitInsn(RETURN)
+		main.visitMaxs(2, 1)
 		main.visitEnd()
+
 		// build bytecodes for each instruction
+
 		`class`.visitEnd()
-		new String(`class`.toByteArray())
+		`class`.toByteArray()
 	}
 	def main(args: Array[String]) {
 		//val filename = args(1)
 		val filename = "Foo.class"
-		val fileStream = new FileWriter(filename);
-		val out = new BufferedWriter(fileStream);
-		out.write(generateMain())
-		out.close()
+		val fileStream = new FileOutputStream(filename);
+		fileStream.write(generateClass())
+		fileStream.close()
 	}
 }
