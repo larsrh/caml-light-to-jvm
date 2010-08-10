ad7605282eaa2fdf56dc8a5d0402d75abd70c904
Marek Kubica <marek@xivilization.net>
2010-08-10 11:22:58 +0200

    Added skeleton of machine that will get compiled into the runtime

---
+++ compiler/src/runtime/Machine.java
@@ -0,0 +1,17 @@
+/*
+ * This file is Java, because it is not an actual part of the compiler
+ * but rather the runtime. It contains an implementation of the MaMa
+ * machine instructions which will get compiled, disassembled and inserted
+ * into the bytecode generator
+ *
+ * Disassemble using
+ *
+ * $ java -cp asm.jar:asm-util.jar org.objectweb.asm.util.ASMifierClassVisitor
+ *   Machine.class
+ */
+
+package runtime;
+
+public class Machine {
+
+}
