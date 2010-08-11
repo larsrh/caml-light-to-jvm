f09247c16362f7a464740858e4a6014a3ce10fae
Marek Kubica <marek@xivilization.net>
2010-08-11 13:30:12 +0200

    Added code which rips out the Java bytecode from the compiler and puts it into a new JAR

--- compiler/src/runtime/JarPacker.scala
+++ compiler/src/runtime/JarPacker.scala
@@ -1,4 +1,5 @@
 package runtime
+import scala.io.Source
 import java.io.FileOutputStream
 import java.util.zip.{ZipOutputStream, ZipEntry}
 
@@ -13,14 +14,43 @@ object JarPacker {
 Main-Class: runtime.Machine""" getBytes)
 	}
 
-	//def copyClasses
+	/* copies all Machine$* stuff that is not to be changed into the JAR */
+	def copyClasses(zip: ZipOutputStream) = {
+		val classes = List("1", // this is an anonymous class that we need to add
+								 "Base", "Closure", "Function", "MachineData", "Raw", "Vector")
 
-	//def injectCode
+		for (entry <- classes) {
+			val `class` = Class.forName("runtime.Machine$" + entry)
+			val className = `class`.getName.replaceAll("\\.", "/") + ".class"
+			val bytecodeStream = `class`.getClassLoader.getResourceAsStream(className)
+
+			val buf = new Array[Byte](1024);
+			zip.putNextEntry(new ZipEntry("runtime/Machine$" + entry + ".class"))
+
+			// holy cow, how much I hate these IOStreams!
+			// Scala makes it even harder to work with
+			var done = false
+			while (!done) {
+				val len = bytecodeStream.read(buf);
+				if (len == -1) {
+					done = true;
+				} else {
+					zip.write(buf, 0, len)
+				}
+			}
+		}
+	}
+
+	/* writes Machine.class */
+	def injectCode(zip: ZipOutputStream) = {
+	}
   
   def main(args: Array[String]): Unit = {
-    val filename = "program.jar"
+    val filename = args(0)
 		val jar = createJar(filename)
 		addManifest(jar)
+		copyClasses(jar)
+		injectCode(jar)
 		jar close
   }
 
